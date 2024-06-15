6ba0d10821b814553bdb217bb8c4050567f2ec72
Taha Hancı <taha.hanci@boun.edu.tr>
2024-06-15 11:45:16 +0300

    highest altitude.

---
+++ src/hncdev/HighestAltitude.java
@@ -0,0 +1,35 @@
+package hncdev;
+
+import java.util.Arrays;
+
+public class HighestAltitude {
+
+//    public int largestAltitude(int[] gain) {
+//        int[] altitudes = new int[gain.length + 1];
+//        altitudes[0] = 0;
+//        for (int i = 1; i < altitudes.length; i++) {
+//            altitudes[i] = gain[i - 1] + altitudes[i - 1];
+//        }
+//        int maxGain = Arrays.stream(altitudes).max().getAsInt();
+//        return maxGain;
+//    }
+
+    public int largestAltitude(int[] gain) {
+        int currentAltitude = 0;
+        int maxAltitude = 0;
+        for (int i = 0; i < gain.length; i++) {
+            currentAltitude += gain[i];
+            if (currentAltitude > maxAltitude) {
+                maxAltitude = currentAltitude;
+            }
+        }
+        return maxAltitude;
+    }
+
+    public static void main(String[] args) {
+        HighestAltitude highestAltitude = new HighestAltitude();
+        int[] gain = {-4,-3,-2,-1,4,3,2};
+        int max = highestAltitude.largestAltitude(gain);
+        System.out.println(max);
+    }
+}
