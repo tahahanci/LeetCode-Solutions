434a19430da75951b059f5ed1112acca276a76d6
Taha Hancı <taha.hanci@boun.edu.tr>
2024-06-21 11:42:06 +0300

    highest altitude description

--- src/hncdev/HighestAltitude.java
+++ src/hncdev/HighestAltitude.java
@@ -4,26 +4,33 @@ import java.util.Arrays;
 
 public class HighestAltitude {
 
-    public int largestAltitude(int[] gain) {
-        int[] altitudes = new int[gain.length + 1];
-        altitudes[0] = 0;
-        for (int i = 1; i < altitudes.length; i++) {
-            altitudes[i] = gain[i - 1] + altitudes[i - 1];
-        }
-        return Arrays.stream(altitudes).max().getAsInt();
-    }
+    /*
+    There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
+    The biker starts his trip on point 0 with altitude equal 0.
+    You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i and i + 1
+    for all (0 <= i < n). Return the highest altitude of a point.
+     */
 
 //    public int largestAltitude(int[] gain) {
-//        int currentAltitude = 0;
-//        int maxAltitude = 0;
-//        for (int i = 0; i < gain.length; i++) {
-//            currentAltitude += gain[i];
-//            if (currentAltitude > maxAltitude) {
-//                maxAltitude = currentAltitude;
-//            }
+//        int[] altitudes = new int[gain.length + 1];
+//        altitudes[0] = 0;
+//        for (int i = 1; i < altitudes.length; i++) {
+//            altitudes[i] = gain[i - 1] + altitudes[i - 1];
 //        }
-//        return maxAltitude;
+//        return Arrays.stream(altitudes).max().getAsInt();
 //    }
+
+    public int largestAltitude(int[] gain) {
+        int currentAltitude = 0;
+        int maxAltitude = 0;
+        for (int i = 0; i < gain.length; i++) {
+            currentAltitude += gain[i];
+            if (currentAltitude > maxAltitude) {
+                maxAltitude = currentAltitude;
+            }
+        }
+        return maxAltitude;
+    }
 
     public static void main(String[] args) {
         HighestAltitude highestAltitude = new HighestAltitude();
